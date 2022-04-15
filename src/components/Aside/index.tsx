46158c5b254f055e1b41c31366a33e6c59d128a4
Felipe Rosa <felipemax.suporteti@gmail.com>
2022-04-15 15:09:45 -0300

    feat: Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+  Container, Header, LogoImg, MenuContainer, MenuItemLink, Title
+} from './styles';
+import { MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp } from 'react-icons/md';
+import logoImg from "../../assets/logo.svg";
 
 const Aside = () => {
   return (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={logoImg} alt="My Wallet Logo" />
+        <Title>My Wallet</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="/dashboard">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+        <MenuItemLink href="/dashboard">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+        <MenuItemLink href="/dashboard">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+        <MenuItemLink href="/dashboard">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 };
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,4 +4,46 @@ export const Container = styled.div`
   grid-area: AS;
   background-color: ${(props) => props.theme.colors.secondary};
   color: ${(props) => props.theme.colors.text};
+  padding-left: 24px;
+  border-right: 1px solid ${({ theme }) => theme.colors.gray};
+  padding: 16px;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  align-items: center;
+`;
+
+export const Title = styled.h3`
+  color: ${({ theme }) => theme.colors.text};
+  margin-left: 8px;
+`;
+
+export const LogoImg = styled.img`
+  height: 40px;
+  width: 40px;
+`;
+
+export const MenuContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+`;
+
+export const MenuItemLink = styled.a`
+  color: ${({ theme }) => theme.colors.text};
+  text-decoration: none;
+  margin: 8px 0;
+  display: flex;
+  align-items: center;
+
+  transition: opacity 0.3s;
+
+  > svg {
+    margin-right: 8px;
+  }
+
+  &:hover {
+    opacity: 0.7;
+  }
 `;
