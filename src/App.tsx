c0e0650b86e9706a778851d65619a5d313c9f30c
Felipe Rosa <felipemax.suporteti@gmail.com>
2022-04-15 10:18:11 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import './styles.scss';
 
 const App = () => {
-  return (
-    <h1>
-      React TypeScript Webpack Starter Template - {process.env.NODE_ENV} {process.env.name}
-    </h1>
-  );
+  return <h1>Dashboard</h1>;
 };
 
 export default App;
